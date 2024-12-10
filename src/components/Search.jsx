9e441cfbe81a779041b227752e3d4071c7867259
mosio14 <mostafa.asadollahy@gmail.com>
2024-12-10 13:34:25 +0100

    change come css in search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,9 +2,9 @@ export default function Search() {
     return (<>
         <div className="flex items-center justify-center">
             <div className="relative">
-                <i className="fas fa-search absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"></i>
+                <i className="fas fa-search absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 "></i>
                 <input
-                    className="pl-10 p-2 rounded-full border-2 border-gray-400 focus:border-gray-700 focus:outline-none"
+                    className="pl-10 p-2 rounded-full border-2 border-gray-400 focus:border-gray-700 focus:outline-none dark:text-black"
                     type="text"
                     name="search-city"
                     placeholder="Search city..."
