5de78365c88b7fa42ef8a076ad14220be65d7a12
mosio14 <mostafa.asadollahy@gmail.com>
2024-12-10 21:25:18 +0100

    use .env file

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export async function getWeatherData(endpoint, place_id, measurementSystem) {
+  const API_KEY = import.meta.env.VITE_API_KEY
+
+
   const options = {
     method: "GET",
     url: `https://ai-weather-by-meteosource.p.rapidapi.com/${endpoint}`,
@@ -11,7 +14,7 @@ export async function getWeatherData(endpoint, place_id, measurementSystem) {
       units: measurementSystem,
     },
     headers: {
-      "x-rapidapi-key": "ff4a6a6ff3mshe84e1ca14cf044fp11406ejsn7bd6e4f7ade2",
+      "x-rapidapi-key": API_KEY,
       "x-rapidapi-host": "ai-weather-by-meteosource.p.rapidapi.com",
     },
   };
@@ -33,7 +36,7 @@ export async function searchPlaces(text) {
       language: "en",
     },
     headers: {
-      "x-rapidapi-key": "ff4a6a6ff3mshe84e1ca14cf044fp11406ejsn7bd6e4f7ade2",
+      "x-rapidapi-key": API_KEY,
       "x-rapidapi-host": "ai-weather-by-meteosource.p.rapidapi.com",
     },
   };
