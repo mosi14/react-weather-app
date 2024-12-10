2578fa01dd151b73ee11b150e9e08b6bd8f1b6be
mosio14 <mostafa.asadollahy@gmail.com>
2024-12-10 17:34:53 +0100

    add search api

--- src/api/index.js
+++ src/api/index.js
@@ -23,3 +23,25 @@ export async function getWeatherData(endpoint, place_id, measurementSystem) {
     console.error(error);
   }
 }
+
+export async function searchPlaces(text) {
+  const options = {
+    method: "GET",
+    url: "https://ai-weather-by-meteosource.p.rapidapi.com/find_places",
+    params: {
+      text,
+      language: "en",
+    },
+    headers: {
+      "x-rapidapi-key": "ff4a6a6ff3mshe84e1ca14cf044fp11406ejsn7bd6e4f7ade2",
+      "x-rapidapi-host": "ai-weather-by-meteosource.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
