93e89b9a8bb02aafe00213f13450f9e7fe0b0599
mosio14 <mostafa.asadollahy@gmail.com>
2024-12-10 13:36:28 +0100

    add some CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,15 @@ import Search from "./Search.jsx";
 import Settings from "./Settings.jsx";
 
 export default function Header() {
-    return (<>
-        <header className=' flex justify-between items-center bg-stone-100 w-full h-1/5 text-center p-5 shadow'>
-            <Place/>
-            <Search/>
-            <Settings/>
-        </header>
-    </>)
+  return (
+    <>
+      <header className=" bg-stone-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 w-full h-1/5 text-center p-5 shadow">
+        <div className="flex justify-between items-center sm:container mx-auto">
+          <Place />
+          <Search />
+          <Settings />
+        </div>
+      </header>
+    </>
+  );
 }
