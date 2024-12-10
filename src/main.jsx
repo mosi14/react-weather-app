b9fb5b8498b23a063846eb9d2224fb20e1fccbf6
mosio14 <mostafa.asadollahy@gmail.com>
2024-12-10 13:32:42 +0100

    add context

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { ThemeProvider } from "./context/theme.context.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { WeatherProvider } from "./context/weather.context.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider>
+      <WeatherProvider>
+        <App />
+      </WeatherProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
