0346e22c5c1492fe2f3c7757cd7ab8b6829770d8
mosio14 <mostafa.asadollahy@gmail.com>
2024-12-10 13:35:43 +0100

    add api adn some DUMMY data

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,25 @@
-import currentWeather from './current-weather'
+import axios from "axios";
 
-function getCurrentWeather() {
-    return currentWeather.current
+export async function getWeatherData(endpoint, place_id, measurementSystem) {
+  const options = {
+    method: "GET",
+    url: `https://ai-weather-by-meteosource.p.rapidapi.com/${endpoint}`,
+    params: {
+      place_id,
+      timezone: "auto",
+      language: "en",
+      units: measurementSystem,
+    },
+    headers: {
+      "x-rapidapi-key": "ff4a6a6ff3mshe84e1ca14cf044fp11406ejsn7bd6e4f7ade2",
+      "x-rapidapi-host": "ai-weather-by-meteosource.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
-export  {getCurrentWeather}
