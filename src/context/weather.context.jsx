920e23573da659d9abd6e94acbfb5cc65d3e9472
mosio14 <mostafa.asadollahy@gmail.com>
2024-12-10 13:31:20 +0100

    add wather context

---
+++ src/context/weather.context.jsx
@@ -0,0 +1,57 @@
+import { createContext, useState, useContext, useEffect } from "react";
+import { DEFAULT_PLACE, MEASUREMENT_SYSTEMS, UNITS } from "../utils/index";
+import { getWeatherData } from "../api";
+
+const WeatherContext = createContext();
+
+function WeatherProvider({ children }) {
+  const [place, setPlace] = useState(DEFAULT_PLACE);
+  const [loading, setLoading] = useState(true);
+  const [currentWeather, setCurrentWeather] = useState({});
+  const [hourlyForecast, setHourlyForecast] = useState([]);
+  const [dailyForecast, setDailyForecast] = useState([]);
+  const [units,setUnits]=useState({})
+  const [measurementSystem, setMeasurementSystem] = useState(
+    MEASUREMENT_SYSTEMS.AUTO
+  );
+
+  useEffect(() => {
+    async function _getWeatherData() {
+      setLoading(true);
+
+      const cw = await getWeatherData("current", place.place_id, "auto");
+      setCurrentWeather(cw.current);
+      setUnits(UNITS[cw.units]);
+
+      const hf = await getWeatherData("hourly", place.place_id, "auto");
+      setHourlyForecast(hf.hourly.data);
+
+      const df = await getWeatherData("daily", place.place_id, "auto");
+      setDailyForecast(df.daily.data);
+
+      setLoading(false);
+    }
+    _getWeatherData();
+  }, [place]);
+
+  return (
+    <WeatherContext.Provider
+      value={{
+        place,
+        setPlace,
+        loading,
+        currentWeather,
+        hourlyForecast,
+        dailyForecast,
+        measurementSystem,
+        setMeasurementSystem,
+        units,
+      }}
+    >
+      {children}
+    </WeatherContext.Provider>
+  );
+}
+
+export { WeatherProvider };
+export const useWeather = () => useContext(WeatherContext);
